d37811f92a904f0f2afa23be3726f4503e53cc05
wk <somnuk.wk@gmail.com>
2020-01-09 12:49:42 +0700

    Reboot

--- src/WinRM/Program.cs
+++ src/WinRM/Program.cs
@@ -1,9 +1,27 @@
 ﻿using System;
+using System.IO;
+using Naos.WinRM;
 
 namespace WinRM {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            // this is the entrypoint to interact with the system (interfaced for testing).
+            var machineManager = new MachineManager(
+                "192.168.33.30",
+                "vagrant",
+                "vagrant".ToSecureString(),
+                autoManageTrustedHosts: true);
+
+            // will perform a user initiated reboot.
+            machineManager.Reboot();
+
+            // can transfer files to AND from the remote server (over WinRM's protocol!).
+            var localFilePath = @"README.md";
+            var fileBytes = File.ReadAllBytes(localFilePath);
+            var remoteFilePath = @"/README.md";
+
+            machineManager.SendFile(remoteFilePath, fileBytes);
+            var downloadedBytes = machineManager.RetrieveFile(remoteFilePath);
         }
     }
 }
